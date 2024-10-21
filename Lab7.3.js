4a4f38da19d8413b66bb607abf81d200910e2495
LyNnisLyNn <132685445+LyNnisLyNn@users.noreply.github.com>
2024-10-21 23:46:23 +0800

    Update Lab7.3.js

--- Lab7.3.js
+++ Lab7.3.js
@@ -1,84 +1,90 @@
-function init()
-{
+function init() {
 
+    // Set width and height of the SVG element
     var w = 300;
     var h = 300;
     var padding = 20;
 
+    // Dataset containing values for apples, oranges, and grapes
     var dataset = [
         { apples: 5, oranges: 10, grapes: 22 },
         { apples: 4, oranges: 12, grapes: 28 },
         { apples: 2, oranges: 19, grapes: 32 },
         { apples: 7, oranges: 23, grapes: 35 },
         { apples: 23, oranges: 17, grapes: 43 }
-        ];
+    ];
 
-    var color = d3.scaleOrdinal(d3.schemeCategory10)
+    // Set color scheme for each category (apples, oranges, grapes)
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
 
+    // Define a stack generator for the dataset with keys corresponding to categories
     var stack = d3.stack()
-                .keys(["apples", "oranges", "grapes"]); 
+                .keys(["apples", "oranges", "grapes"]);
 
+    // Create stacked data series based on the dataset
     var series = stack(dataset);
 
+    // Define the x-scale as a band scale, mapping each data point to a separate bar
     var xScale = d3.scaleBand()
-    .domain(d3.range(dataset.length))
-    .rangeRound([padding, w - padding])
-    .padding(0.05); 
+    .domain(d3.range(dataset.length)) // Sets domain as index of dataset elements
+    .rangeRound([padding, w - padding]) // Spans from padding to width minus padding
+    .padding(0.05); // Adds space between bars
 
+    // Define the y-scale as a linear scale to map total fruit values to heights in the SVG
     var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d)
-        {
-            return d.apples + d.oranges + d.grapes;
+    .domain([0, d3.max(dataset, function(d) { 
+            return d.apples + d.oranges + d.grapes; // Sum of fruits in each data point
         })
     ])
-    .range([h - padding, padding]);
+    .range([h - padding, padding]); // Inverse range so lower values are at the bottom
 
+    // Define the x-axis with tick marks based on xScale
     var xAxis = d3.axisBottom()
-    .ticks(5)
+    .ticks(5) // Set number of ticks to 5
     .scale(xScale);
 
+    // Define the y-axis with tick marks based on yScale
     var yAxis = d3.axisLeft()
-    .ticks(5)
+    .ticks(5) // Set number of ticks to 5
     .scale(yScale);
 
     var svg = d3.select("#stack_bar")
                 .append("svg")
-                .attr("width", w)
-                .attr("height", h);
+                .attr("width", w) 
+                .attr("height", h); 
 
+    // Create group elements for each series (apples, oranges, grapes)
     var groups = svg.selectAll("g")
-    .data(series)
+    .data(series) // Bind stacked series data
     .enter()
     .append("g")
-    .style("fill", function(d, i)
-    {
-        return color(i);
+    .style("fill", function(d, i) {
+        return color(i); // Assign color to each category based on index
     });
 
     var rects = groups.selectAll("rect")
-        .data(function(d) {return d;})
+        .data(function(d) { return d; }) // Bind data within each series
         .enter()
         .append("rect")
-        .attr("x", function(d, i)
-        {
-            return xScale(i);
+        .attr("x", function(d, i) {
+            return xScale(i); // Set x-position based on the index
         })
-        .attr("y", function(d, i)
-        {
-            return yScale(d[1]);
+        .attr("y", function(d, i) {
+            return yScale(d[1]); // Set y-position based on the top value in the stack
         })
-        .attr("height", function(d)
-        {
-            return yScale(d[0]) - yScale(d[1]);
+        .attr("height", function(d) {
+            return yScale(d[0]) - yScale(d[1]); // Calculate height from top to bottom of stack
         })
-        .attr("width", xScale.bandwidth());
+        .attr("width", xScale.bandwidth()); // Set the width of each bar
 
+    // Append x-axis to the SVG, translate it to the bottom of the chart
     svg.append("g")
         .attr("transform", "translate(0, " + (h - padding) + ")")
         .call(xAxis);
 
-     svg.append("g")
-        .attr("transform", "translate(" + (padding) + ", 0)")
+    // Append y-axis to the SVG, position it on the left side
+    svg.append("g")
+        .attr("transform", "translate(" + padding + ", 0)")
         .call(yAxis);
 
 }
