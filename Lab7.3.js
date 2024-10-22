5ba76f4991b075b14c930803f476b075247019dd
LyNnisLyNn <132685445+LyNnisLyNn@users.noreply.github.com>
2024-10-22 08:18:39 +0800

    Update Lab7.3.js

--- Lab7.3.js
+++ Lab7.3.js
@@ -1,5 +1,4 @@
 function init() {
-
     // Set width and height of the SVG element
     var w = 300;
     var h = 300;
@@ -26,41 +25,41 @@ function init() {
 
     // Define the x-scale as a band scale, mapping each data point to a separate bar
     var xScale = d3.scaleBand()
-    .domain(d3.range(dataset.length)) // Sets domain as index of dataset elements
-    .rangeRound([padding, w - padding]) // Spans from padding to width minus padding
-    .padding(0.05); // Adds space between bars
+        .domain(d3.range(dataset.length)) // Sets domain as index of dataset elements
+        .rangeRound([padding, w - padding]) // Spans from padding to width minus padding
+        .padding(0.05); // Adds space between bars
 
     // Define the y-scale as a linear scale to map total fruit values to heights in the SVG
     var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { 
-            return d.apples + d.oranges + d.grapes; // Sum of fruits in each data point
-        })
-    ])
-    .range([h - padding, padding]); // Inverse range so lower values are at the bottom
+        .domain([0, d3.max(dataset, function(d) { 
+                return d.apples + d.oranges + d.grapes; // Sum of fruits in each data point
+            })
+        ])
+        .range([h - padding, padding]); // Inverse range so lower values are at the bottom
 
     // Define the x-axis with tick marks based on xScale
     var xAxis = d3.axisBottom()
-    .ticks(5) // Set number of ticks to 5
-    .scale(xScale);
+        .ticks(5) // Set number of ticks to 5
+        .scale(xScale);
 
     // Define the y-axis with tick marks based on yScale
     var yAxis = d3.axisLeft()
-    .ticks(5) // Set number of ticks to 5
-    .scale(yScale);
+        .ticks(5) // Set number of ticks to 5
+        .scale(yScale);
 
     var svg = d3.select("#stack_bar")
                 .append("svg")
-                .attr("width", w) 
+                .attr("width", w + 100) // Increase width to accommodate legend
                 .attr("height", h); 
 
     // Create group elements for each series (apples, oranges, grapes)
     var groups = svg.selectAll("g")
-    .data(series) // Bind stacked series data
-    .enter()
-    .append("g")
-    .style("fill", function(d, i) {
-        return color(i); // Assign color to each category based on index
-    });
+        .data(series) // Bind stacked series data
+        .enter()
+        .append("g")
+        .style("fill", function(d, i) {
+            return color(i); // Assign color to each category based on index
+        });
 
     var rects = groups.selectAll("rect")
         .data(function(d) { return d; }) // Bind data within each series
@@ -87,6 +86,36 @@ function init() {
         .attr("transform", "translate(" + padding + ", 0)")
         .call(yAxis);
 
+    // Add legend to the chart
+    var categories = ["apples", "oranges", "grapes"];
+
+    var legend = svg.selectAll(".legend")
+        .data(categories)
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) {
+            return "translate(" + (w + 20) + "," + (i * 20 + padding) + ")";
+        });
+
+    // Add colored rectangles to legend
+    legend.append("rect")
+        .attr("x", 0)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function(d, i) {
+            return color(i);
+        });
+
+    // Add text labels to legend
+    legend.append("text")
+        .attr("x", 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(function(d) {
+            return d;
+        });
 }
 
 window.onload = init;
